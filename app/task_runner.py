b7744882d1b3bf18cf787d6e513e397a1f50eb2e
Stan Andrei Razvan <andreirazvan922@gmail.com>
2025-03-26 17:19:13 +0200

    some todos

--- app/task_runner.py
+++ app/task_runner.py
@@ -1,6 +1,9 @@
 from queue import Queue
 from threading import Thread, Event
+import os
+import json
 import time
+
 
 class ThreadPool:
     def __init__(self):
@@ -13,11 +16,42 @@ class ThreadPool:
         #   * create more threads than the hardware concurrency allows
         #   * recreate threads for each task
         # Note: the TP_NUM_OF_THREADS env var will be defined by the checker
-        pass
+        if 'TP_NUM_OF_THREADS' in os.environ:
+            self.num_threads = int(os.environ['TP_NUM_OF_THREADS'])
+        else:
+            self.num_threads = os.cpu_count()
+        self.task_queue = Queue()
+        self.threads = []
+        self.shutdown_event = Event()
+        self._create_threads()
+        
+    def _create_threads(self):
+        for _ in range(self.num_threads):
+            thread = TaskRunner()
+            self.threads.append(thread)
+            thread.start()
+
+    def add_task(self, task):
+        # Add task to the queue
+        self.task_queue.put(task)
+
+    def wait_completion(self):
+        # Wait for all tasks to be completed
+        self.task_queue.join()
+
+    def shutdown(self):
+        # Shutdown the ThreadPool
+        # This should also stop all the threads in the pool
+        self.shutdown_event.set()
+        for _ in range(self.num_threads):
+            self.task_queue.put(None)
+        for thread in self.threads:
+            thread.join()
+        self.task_queue.join()
 
 class TaskRunner(Thread):
     def __init__(self):
-        # TODO: init necessary data structures
+        # TODO: init necessary data structures 
         pass
 
     def run(self):
